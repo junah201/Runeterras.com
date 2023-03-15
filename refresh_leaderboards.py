3e20a93a3b4e5390bcec465386bae90a0a6e588c
junah201 <junah.dev@gmail.com>
2023-03-15 11:29:41 +0900

    feat : 디스코드 웹훅 로그 추가

--- refresh_leaderboards.py
+++ refresh_leaderboards.py
@@ -2,12 +2,23 @@ import requests
 import os
 import models
 import database
-from typing import List
+from typing import List, Dict
+from datetime import datetime
 
 RIOT_API_KEY = os.environ.get("RIOT_API_KEY")
+DISCORD_WEBHOOKS_REFRESH_LEADERBOARDS_LOG = os.environ.get(
+    "DISCORD_WEBHOOKS_REFRESH_LEADERBOARDS_LOG")
 
 
-def lambda_handler(event, context):
+def refresh_leaderboards() -> Dict:
+    log = {
+        "start": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "new master players": [],
+        "total master players": 0,
+        "end": None,
+        "rate limit": False,
+    }
+
     res = requests.get(
         "https://sea.api.riotgames.com/lor/ranked/v1/leaderboards",
         headers={
@@ -18,10 +29,12 @@ def lambda_handler(event, context):
     )
 
     if res.status_code != 200:
-        print(res.text)
-        return
+        if res.status_code == 429:
+            log["rate limit"] = True
+        return log
 
     players: List[str] = [player["name"] for player in res.json()["players"]]
+    log["total master players"] = len(players)
 
     db = database.get_db()
 
@@ -39,15 +52,63 @@ def lambda_handler(event, context):
 
     for player in players:
         if player not in db_master_player_names:
-            print(player)
+            log["new master players"].append(f"**{player}**\n")
             db_player = models.Player(
                 game_name=player,
                 is_master=True,
             )
             db.add(db_player)
 
-    print("end")
     db.commit()
+    return log
 
 
-lambda_handler(None, None)
+def lambda_handler(event, context):
+
+    log = refresh_leaderboards()
+    log["end"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    log = {
+        "start": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "new master players": [],
+        "total master players": 0,
+        "end": None,
+        "rate limit": False,
+    }
+
+    success_color = 0x2ECC71
+    error_color = 0xE74C3C
+
+    if log["rate limit"]:
+        color = error_color
+    else:
+        color = success_color
+
+    data = {
+        "content": "",
+        "embeds": [
+            {
+                "title": "Refresh Leaderboards LOG",
+                "description": f"""
+                총 `{log["total master players"]}`명의 마스터 플레이어가 있습니다.
+
+                새로운 마스터 플레이어 `{len(log["new master players"])}`명을 발견했습니다.
+                {"```" + "".join([f"{player}" for player in log["new master players"]]) + "```" if log["new players"] else ""}
+
+                rate limit : `{log["rate limit"]}`
+
+                시작 : `{log["start"]}`
+                종료 : `{log["end"]}`
+                """,
+                "color": color,
+                "footer": {
+                    "text": str(log["start"])
+                }
+            }
+        ]
+    }
+
+    requests.post(
+        DISCORD_WEBHOOKS_REFRESH_LEADERBOARDS_LOG,
+        json=data
+    )
