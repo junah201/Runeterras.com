efc17ac9dbc18b2f9947bdea8e14b1b4700c4853
junah201 <junah.dev@gmail.com>
2023-03-18 20:06:29 +0900

    fix : duplicate match id data

--- Backend/save_match_data.py
+++ Backend/save_match_data.py
@@ -72,111 +72,109 @@ def get_match_data_datail() -> Tuple[List, Dict]:
     sqs_client = boto3.resource('sqs', region_name='ap-northeast-2')
     match_id_sqs = sqs_client.get_queue_by_name(
         QueueName='LOR__match-data-queue')
-
-    match_ids = []
-
-    for _ in range(0, max_number // 10):
-        match_ids.extend(
-            match_id_sqs.receive_messages(
-                MessageAttributeNames=['All'],
-                MaxNumberOfMessages=10,
-                WaitTimeSeconds=10
-            )
-        )
-
-    log["total receive data count"] = len(match_ids)
-
     new_player_puuid_sqs = sqs_client.get_queue_by_name(
         QueueName='LOR__new-palyer-puuid-queue')
 
-    match_id_queue = Queue()
-    for match_id in match_ids:
-        match_id_queue.put(match_id)
-
-    while not match_id_queue.empty():
-        match_id = match_id_queue.get()
-
-        match_res = requests.get(
-            f"https://apac.api.riotgames.com/lor/match/v1/matches/{match_id}",
-            headers={
-                "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-                "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                "X-Riot-Token": RIOT_API_KEY,
-            },
-            timeout=1
+    for _ in range(0, max_number, 10):
+        match_ids = match_id_sqs.receive_messages(
+            MessageAttributeNames=['All'],
+            MaxNumberOfMessages=10,
+            WaitTimeSeconds=10
         )
 
-        # rate limit
-        if match_res.status_code == 429:
-            log["rate limit"] = True
+        if not match_ids:
             break
 
-        # error
-        if match_res.status_code != 200:
-            log["error"] = match_res.text
-            break
+        log["total receive data count"] += len(match_ids)
 
-        match_data = match_res.json()
+        match_id_queue = Queue()
+        for match_id in match_ids:
+            match_id_queue.put(match_id)
 
-        if match_data["info"]["game_type"] != "Ranked":
-            continue
+        print(match_id_queue.qsize())
 
-        new_last_matched_at = datetime.strptime(
-            match_data["info"]["game_start_time_utc"][:26], "%Y-%m-%dT%H:%M:%S.%f")
-
-        tmp = dict()
-        tmp["data_version"] = match_data["metadata"]["data_version"]
-        tmp["match_id"] = match_data["metadata"]["match_id"]
-        tmp["game_mode"] = match_data["info"]["game_mode"]
-        tmp["game_type"] = match_data["info"]["game_type"]
-        tmp["game_start_time_utc"] = match_data["info"]["game_start_time_utc"]
-        tmp["game_version"] = match_data["info"]["game_version"]
-
-        for player in match_data["info"]["players"]:
-            tmp[f"{player['game_outcome']}_user_puuid"] = player["puuid"]
-            tmp[f"{player['game_outcome']}_user_deck_id"] = player["deck_id"]
-            tmp[f"{player['game_outcome']}_user_deck_code"] = player["deck_code"]
-            tmp[f"{player['game_outcome']}_user_order_of_play"] = player["order_of_play"]
-
-        row = []
-        for header in header_row:
-            row.append(str(tmp.get(header, "")))
-        tmp = ", ".join(row) + "\n"
-        match_datas.append(tmp)
-
-        for player_puuid in match_data["info"]["participants"]:
-            db_player: models.Player = db.query(models.Player).filter(
-                models.Player.puuid == player_puuid).first()
-
-            if not db_player:
-                new_player_puuid_sqs.send_message(
-                    DelaySeconds=10,
-                    MessageBody=(
-                        f"{player_puuid}\n{match_id}\n{new_last_matched_at}"
-                    )
-                )
-
-            db_player.last_matched_at = max(
-                db_player.last_matched_at or datetime.min, new_last_matched_at)
-            db.commit()
-        log["total save data count"] += 1
-
-    # fail to save data send sqs
-    if match_id_queue.qsize() > 0:
         while not match_id_queue.empty():
             match_id = match_id_queue.get()
-            match_id_sqs.send_message(
-                DelaySeconds=10,
-                MessageBody=(
-                    f"{match_id}"
-                )
+
+            print("match id", match_id.body)
+
+            match_res = requests.get(
+                f"https://apac.api.riotgames.com/lor/match/v1/matches/{match_id.body}",
+                headers={
+                    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+                    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "X-Riot-Token": RIOT_API_KEY,
+                },
+                timeout=1
             )
+
+            # rate limit
+            if match_res.status_code == 429:
+                print("rate limit")
+                log["rate limit"] = True
+                break
+
+            match_id.delete()
+
+            # error
+            if match_res.status_code != 200:
+                log["error"] += match_res.text + "\n"
+                print("error : ", match_res.text)
+                break
+
+            match_data = match_res.json()
+
+            if match_data["info"]["game_type"] != "Ranked":
+                print("is not rank match")
+                continue
+
+            new_last_matched_at = datetime.strptime(
+                match_data["info"]["game_start_time_utc"][:26], "%Y-%m-%dT%H:%M:%S.%f")
+
+            tmp = dict()
+            tmp["data_version"] = match_data["metadata"]["data_version"]
+            tmp["match_id"] = match_data["metadata"]["match_id"]
+            tmp["game_mode"] = match_data["info"]["game_mode"]
+            tmp["game_type"] = match_data["info"]["game_type"]
+            tmp["game_start_time_utc"] = match_data["info"]["game_start_time_utc"]
+            tmp["game_version"] = match_data["info"]["game_version"]
+
+            for player in match_data["info"]["players"]:
+                tmp[f"{player['game_outcome']}_user_puuid"] = player["puuid"]
+                tmp[f"{player['game_outcome']}_user_deck_id"] = player["deck_id"]
+                tmp[f"{player['game_outcome']}_user_deck_code"] = player["deck_code"]
+                tmp[f"{player['game_outcome']}_user_order_of_play"] = player["order_of_play"]
+
+            row = []
+            for header in header_row:
+                row.append(str(tmp.get(header, "")))
+            tmp = ", ".join(row) + "\n"
+            match_datas.append(tmp)
+
+            for player_puuid in match_data["metadata"]["participants"]:
+                db_player: models.Player = db.query(models.Player).filter(
+                    models.Player.puuid == player_puuid).first()
+
+                if not db_player:
+                    new_player_puuid_sqs.send_message(
+                        DelaySeconds=10,
+                        MessageBody=(
+                            f"{player_puuid}\n{match_id}\n{new_last_matched_at}"
+                        )
+                    )
+                    continue
+
+                db_player.last_matched_at = max(
+                    db_player.last_matched_at or datetime.min, new_last_matched_at)
+                db.commit()
+            log["total save data count"] += 1
 
     return match_datas, log
 
 
 def save_match_data(match_datas: List[str]) -> None:
     if not match_datas:
+        print("no data")
         return
 
     tmp = ", ".join(header_row) + "\n"
@@ -188,7 +186,7 @@ def save_match_data(match_datas: List[str]) -> None:
     s3.put_object(
         Bucket="lor-match-data",
         Key=f"{datetime.utcnow().strftime('year=%Y/month=%m/day=%d/hour=%H')}/match_data-1-{uuid.uuid4()}.csv.gz",
-        Body=gzip.compress(match_datas.encode("utf-8"))
+        Body=gzip.compress(tmp.encode("utf-8"))
     )
 
 
