2d203f4674653625a7e966edd8626e75c262d282
junah201 <junah.dev@gmail.com>
2023-03-18 17:49:13 +0900

    feat : Change the collected match id to send to SQS

--- Backend/check_match.py
+++ Backend/check_match.py
@@ -33,18 +33,14 @@ header_row = [
 ]
 
 
-def check_match() -> Tuple[str, Dict]:
+def check_match() -> Dict:
     log = {
         "start": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "total checked": 0,
-        "new players": [],
-        "updated players": [],
         "new ranked matches": 0,
         "end": None,
         "rate limit": False,
     }
-
-    match_datas = ", ".join(header_row) + "\n"
 
     db = database.get_db()
 
@@ -52,8 +48,9 @@ def check_match() -> Tuple[str, Dict]:
         .filter(models.Player.is_master == True, models.Player.puuid != None) \
         .order_by(models.Player.last_checked_at.asc(), models.Player.last_matched_at.desc()).all()
 
+    new_match_ids = []
+
     for db_master_player in db_master_players_puuid:
-        log["total checked"] += 1
         print("start", db_master_player.puuid)
         res = requests.get(
             f"https://apac.api.riotgames.com/lor/match/v1/matches/by-puuid/{db_master_player.puuid}/ids",
@@ -70,13 +67,14 @@ def check_match() -> Tuple[str, Dict]:
             if res.status_code == 429:
                 print("Too many requests")
                 log["rate limit"] = True
-                return match_datas, log
+                break
             continue
+
+        log["total checked"] += 1
 
         match_ids = res.json()
 
         new_last_match_id: Optional[str] = None
-        new_last_matched_at = db_master_player.last_matched_at or datetime.min
         if match_ids:
             new_last_match_id = match_ids[0]
 
@@ -86,125 +84,35 @@ def check_match() -> Tuple[str, Dict]:
                 break
 
             print("match_id", match_id)
-
-            match_res = requests.get(
-                f"https://apac.api.riotgames.com/lor/match/v1/matches/{match_id}",
-                headers={
-                    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-                    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                    "X-Riot-Token": RIOT_API_KEY,
-                },
-                timeout=3
-            )
-
-            if match_res.status_code != 200:
-                print(match_res.text)
-                if match_res.status_code == 429:
-                    print("Too many requests")
-                    log["rate limit"] = True
-                    return match_datas, log
-                continue
-
-            match_data = match_res.json()
-
-            new_last_matched_at = max(new_last_matched_at, datetime.strptime(
-                match_data["info"]["game_start_time_utc"][:26], "%Y-%m-%dT%H:%M:%S.%f"))
-
-            if match_data["info"]["game_type"] != "Ranked":
-                continue
-
-            tmp = dict()
-            tmp["data_version"] = match_data["metadata"]["data_version"]
-            tmp["match_id"] = match_data["metadata"]["match_id"]
-            tmp["game_mode"] = match_data["info"]["game_mode"]
-            tmp["game_type"] = match_data["info"]["game_type"]
-            tmp["game_start_time_utc"] = match_data["info"]["game_start_time_utc"]
-            tmp["game_version"] = match_data["info"]["game_version"]
-
-            for player in match_data["info"]["players"]:
-                tmp[f"{player['game_outcome']}_user_puuid"] = player["puuid"]
-                tmp[f"{player['game_outcome']}_user_deck_id"] = player["deck_id"]
-                tmp[f"{player['game_outcome']}_user_deck_code"] = player["deck_code"]
-                tmp[f"{player['game_outcome']}_user_order_of_play"] = player["order_of_play"]
-
-            row = []
-            for header in header_row:
-                row.append(str(tmp.get(header, "")))
-            tmp = ", ".join(row) + "\n"
-            match_datas += tmp
-
-            log["new ranked matches"] += 1
-
-            for puuid in match_data["metadata"]["participants"]:
-                if puuid == db_master_player.puuid:
-                    continue
-
-                # 이미 DB에 수집된 puuid인지 확인
-                is_collected = db.query(literal(True)).filter(db.query(models.Player).filter(
-                    models.Player.puuid == puuid
-                ).exists()).scalar()
-                print(is_collected)
-                if is_collected == True:
-                    continue
-
-                account_res = requests.get(
-                    f"https://asia.api.riotgames.com/riot/account/v1/accounts/by-puuid/{puuid}",
-                    headers={
-                        "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-                        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-                        "X-Riot-Token": RIOT_API_KEY,
-                    },
-                    timeout=3
-                )
-                if account_res.status_code != 200:
-                    print(account_res.text)
-                    if account_res.status_code == 429:
-                        print("Too many requests")
-                        return match_datas, log
-                    continue
-                account_data = account_res.json()
-
-                # 상대가 마스터이고, DB에 수집된 puuid가 없지만 이름이 같은 마스터 유저가 있는지 확인
-                db_new_player: models.Player = db.query(models.Player).filter(
-                    models.Player.game_name == account_data["gameName"], models.Player.puuid == None).first()
-
-                if db_new_player:
-                    print("update", db_new_player.game_name, puuid)
-                    log["updated players"].append(
-                        f"{db_new_player.game_name}\n")
-
-                    db_new_player.puuid = puuid
-                    db_new_player.game_name = account_data["gameName"]
-                    db_new_player.tag_line = account_data["tagLine"]
-
-                    db.commit()
-                    continue
-
-                # 상대가 DB에 아예 없으면 (다이아)
-                print("new", account_data["gameName"], puuid)
-                log["new players"].append(
-                    f"{account_data['gameName']}\n")
-                db_new_player = models.Player(
-                    puuid=puuid,
-                    game_name=account_data["gameName"],
-                    tag_line=account_data["tagLine"],
-                    is_master=False
-                )
-                db.add(db_new_player)
-                db.commit()
+            new_match_ids.append(match_id)
 
         db_master_player.last_matched_game_id = new_last_match_id
-        db_master_player.last_matched_at = new_last_matched_at
         db_master_player.last_checked_at = datetime.utcnow()
         db.commit()
         print("end", db_master_player.puuid)
 
+    log["new ranked matches"] = len(new_match_ids)
+
+    sqs_client = boto3.resource('sqs', region_name='ap-northeast-2')
+    match_id_sqs = sqs_client.get_queue_by_name(
+        QueueName='LOR__match-data-queue')
+
+    for idx in range(0, len(new_match_ids), 10):
+        match_id_sqs.send_messages(
+            Entries=[
+                {
+                    "Id": str(uuid.uuid4()),
+                    "MessageBody": match_id
+                } for match_id in new_match_ids[idx:idx+10]
+            ]
+        )
+
     db.commit()
-    return match_datas, log
+    return log
 
 
 def lambda_handler(event, context):
-    match_datas, log = check_match()
+    log = check_match()
     log["end"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     success_color = 0x2ECC71
@@ -223,12 +131,6 @@ def lambda_handler(event, context):
                 "description": f"""
                 총 `{log["total checked"]}`명의 마스터 플레이어의 매치를 수집했습니다.
 
-                새로운 플레이어 `{len(log["new players"])}`명을 발견했습니다.
-                {"```" + "".join([f"{player}" for player in log["new players"]]) + "```" if log["new players"] else ""}
-
-                기존 마스터 플레이어 `{len(log["updated players"])}`명의 puuid를 수집했습니다.
-                {"```" + "".join([f"{player}" for player in log["updated players"]]) + "```" if log["updated players"] else ""}
-
                 새로운 랭크 매치 `{log["new ranked matches"]}`개를 발견했습니다.
 
                 rate limit : `{log["rate limit"]}`
@@ -243,13 +145,6 @@ def lambda_handler(event, context):
             }
         ]
     }
-
-    s3 = boto3.client('s3')
-    s3.put_object(
-        Bucket="lor-match-data",
-        Key=f"{datetime.utcnow().strftime('year=%Y/month=%m/day=%d/hour=%H')}/match_data-1-{uuid.uuid4()}.csv.gz",
-        Body=gzip.compress(match_datas.encode("utf-8"))
-    )
 
     requests.post(
         DISCORD_WEBHOOKS_CHECK_MATCH_LOG,
