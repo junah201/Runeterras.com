0136ca95fa20f112a3356b4aa5427c892da267b6
junah201 <junah.dev@gmail.com>
2023-03-25 14:52:43 +0900

    build : Add CI CD for get_all_version_data.py

--- Backend/api/v1/get_all_version_data.py
+++ Backend/api/v1/get_all_version_data.py
@@ -1,12 +1,7 @@
-import requests
-import os
 import models
 import database
-from datetime import datetime
-from typing import Dict, List
-import boto3
-from sqlalchemy import literal
-from sqlalchemy.sql import exists
+from typing import List
+import json
 
 
 def lambda_handler(event, context):
@@ -15,13 +10,15 @@ def lambda_handler(event, context):
         models.GameVersion.created_at.desc()).all()
 
     return {
-        "statusCode": 200,
-        "body": [
-            {
-                "game_version": db_game_version.game_version,
-                "total_match_count": db_game_version.total_match_count,
-                "created_at": db_game_version.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                "updated_at": db_game_version.updated_at.strftime("%Y-%m-%d %H:%M:%S")
-            } for db_game_version in db_game_versions
-        ]
+        "statusCode": "200",
+        "body": json.dumps(
+            [
+                {
+                    "game_version": db_game_version.game_version,
+                    "total_match_count": db_game_version.total_match_count,
+                    "created_at": db_game_version.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                    "updated_at": db_game_version.updated_at.strftime("%Y-%m-%d %H:%M:%S")
+                } for db_game_version in db_game_versions
+            ]
+        )
     }
